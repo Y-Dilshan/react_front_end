28d61c8a3c26eb1d167acebe20aef52d1770de29
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-02-26 13:52:11 +0530

    created jsx files

---
+++ react_front_end/src/components/productCard.jsx
@@ -0,0 +1,9 @@
+export default function ProductCard(){
+    return(
+        <div>
+            <h1>Product 01</h1>
+            <h2>Price: $100</h2>
+            <button>Buy Now</button>
+        </div>
+    )
+}
