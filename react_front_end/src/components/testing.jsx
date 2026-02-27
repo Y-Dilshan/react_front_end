b7eeac398bf88b07242d4487fc0ec181d7d4c2da
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-02-27 22:41:06 +0530

    added styles

--- react_front_end/src/components/testing.jsx
+++ react_front_end/src/components/testing.jsx
@@ -1,7 +1,11 @@
+import "./testing.css"
+
 export default function Testing(){
     return(
-        <div>
-            Testing 
-        </div>
+        <div className="background">
+            <button>-</button>
+            <span>0</span>
+            <button>+</button>
+       </div>
     )
 }
