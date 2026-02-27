791163700a630bb4f4f7d86a447c37818ccc9895
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-02-27 23:35:14 +0530

    started to learn react hooks useState

--- react_front_end/src/components/testing.jsx
+++ react_front_end/src/components/testing.jsx
@@ -1,11 +1,26 @@
 import "./testing.css"
+import { useState } from "react"
 
 export default function Testing(){
+
+    const [count, setCount] = useState(0)
+
+    function increment(){
+        setCount(count + 1)
+    }
+
+    function decrement(){
+        setCount(count - 1)
+    }
+
     return(
         <div className="background">
-            <button>-</button>
-            <span>0</span>
-            <button>+</button>
+
+            <button onClick={decrement}>-</button>
+
+            <span>{count}</span>
+
+            <button onClick={increment}>+</button>
        </div>
     )
 }
